de7b621974a0dcff4b1d48f9333a6d2dc8005990
ThesplumCoder <michin505@hotmail.com>
2023-12-07 04:50:44 -0500

    Deshabilitado el control de CORS para el controlador de los huespedes, las tarjetas RFID y las habitaciones

--- src/main/java/com/github/thesplum/hoteliotserver/controllers/RfidCardController.java
+++ src/main/java/com/github/thesplum/hoteliotserver/controllers/RfidCardController.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.github.thesplum.hoteliotserver.repositories.RfidCardRepository;
  * RfidCardController
  */
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/rfid-card")
 public class RfidCardController {
 
